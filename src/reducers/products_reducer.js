69daf6ce7e9bfdddaef233bf196470470221a4e1
Code Green <nmacharashvili171993@gmail.com>
2025-02-28 04:16:47 +0400

    first commit

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -7,32 +7,49 @@ import {
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
   GET_SINGLE_PRODUCT_ERROR,
-} from '../actions'
+} from "../actions";
 
 const products_reducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case SIDEBAR_OPEN:
-      return {...state, isSidebarOpen:true}
+      return { ...state, isSidebarOpen: true };
     case SIDEBAR_CLOSE:
-      return {...state, isSidebarOpen:false}
+      return { ...state, isSidebarOpen: false };
     case GET_PRODUCTS_BEGIN:
-      return {...state, products_loading:true}
+      return { ...state, products_loading: true };
     case GET_PRODUCTS_SUCCESS:
-      const featured_products = action.payload.filter(product => {
-        return product.featured === true 
-      })
-      return {...state, products_loading:false, products: action.payload, featured_products: featured_products}
+      const featured_products = action.payload.filter((product) => {
+        return product.featured === true;
+      });
+      return {
+        ...state,
+        products_loading: false,
+        products: action.payload,
+        featured_products: featured_products,
+      };
     case GET_PRODUCTS_ERROR:
-      return {...state, products_loading:false, error:true}
+      return { ...state, products_loading: false, error: true };
     case GET_SINGLE_PRODUCT_BEGIN:
-      return {...state, single_product_loading:true, single_product_error:false}
+      return {
+        ...state,
+        single_product_loading: true,
+        single_product_error: false,
+      };
     case GET_SINGLE_PRODUCT_SUCCESS:
-      return {...state, single_product_loading:false, single_product:action.payload}  
+      return {
+        ...state,
+        single_product_loading: false,
+        single_product: action.payload,
+      };
     case GET_SINGLE_PRODUCT_ERROR:
-      return {...state, single_product_loading:false, single_product_error:true}
-      default:
-    throw new Error(`No Matching "${action.type}" - action type`)
+      return {
+        ...state,
+        single_product_loading: false,
+        single_product_error: true,
+      };
+    default:
+      throw new Error(`No Matching "${action.type}" - action type`);
   }
-}
+};
 
-export default products_reducer
+export default products_reducer;
